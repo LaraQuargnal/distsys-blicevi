1d4b1fa42f7ac0e992cb427bff6e25839c15e3a5
LaraQuargnal <116004169+LaraQuargnal@users.noreply.github.com>
2023-01-13 19:46:16 +0100

    Zadatak 1

--- 01-zadaci/zadatak1.py
+++ 01-zadaci/zadatak1.py
@@ -1,1 +1,5 @@
-
+def f1(predmeti):
+    assert isinstance(predmeti, list) and all(isinstance(s, str) for s in predmeti), "Input must be a list of strings."
+    return {k: v[::-1] for k, v in enumerate(predmeti)}
+  
+print(f1(["Stol", "Stolica", "Krevet", "Fotelja"]))
