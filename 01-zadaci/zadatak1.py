3d7357469ad6976be7a04520f30b8e79a0df90d9
LaraQuargnal <116004169+LaraQuargnal@users.noreply.github.com>
2023-01-13 19:47:07 +0100

    Update zadatak1.py

--- 01-zadaci/zadatak1.py
+++ 01-zadaci/zadatak1.py
@@ -1,5 +1,5 @@
 def f1(predmeti):
-    assert isinstance(predmeti, list) and all(isinstance(s, str) for s in predmeti), "Input must be a list of strings."
+    assert isinstance(predmeti, list) and all(isinstance(s, str) for s in predmeti), "U listi moraju biti svi stringovi."
     return {k: v[::-1] for k, v in enumerate(predmeti)}
   
 print(f1(["Stol", "Stolica", "Krevet", "Fotelja"]))
